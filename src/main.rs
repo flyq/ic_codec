4c7451d3a771025451f4e26fb111b6241d863a5f
flyq <flyq951@gmail.com>
2021-08-08 07:25:36 +0000

    fix error

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,23 @@ const CRC_LENGTH_IN_BYTES: usize = 4;
 fn main() {
     // ledger, ryjl3-tyaaa-aaaaa-aaaba-cai
     let ledger_canister_id: CanisterId = CanisterId::from_u64(2); 
-    let ledger_principal_id: PrincipalId = ledger_canister_id.get_ref();
+    let ledger_principal_id: PrincipalId = *ledger_canister_id.get_ref();
     let ledger_account_id: AccountIdentifier = AccountIdentifier::from(ledger_principal_id);
     let ledger_subaccount: Subaccount = Subaccount::from(&ledger_principal_id);
 
     let ledger_canister_id_1: CanisterId = canister_from_str("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap();
-    assert!(ledger_canister_id, ledger_canister_id_1);
-    println!("Ledger canister id: {:?},\nledger canister id slice: {:?},\nprincipal id: {:?},\nprincipal id slice: {:?},\naccount id: {:?},\naccount id slice: {:?},\naccount id hex string: {:?},\nsubaccount: {:?},\nsubaccount slice: {:?},\n", 
-                 ledger_canister_id, ledger_canister_id.as_ref(), ledger_principal_id, ledger_principal_id.as_slice(), ledger_account_id, ledger_account_id.to_vec(), ledger_account_id.to_hex(), ledger_subaccount, Vec::from(ledger_subaccount));
+    assert_eq!(ledger_canister_id, ledger_canister_id_1);
+    println!("Ledger canister id: {:?},\nprincipal id: {:?},\nprincipal id slice: {:?},\naccount id: {:?},\naccount id slice: {:?},\naccount id hex string: {:?},\nsubaccount: {:?},\nsubaccount slice: {:?},\n", 
+                 ledger_canister_id, ledger_principal_id, ledger_principal_id.as_slice(), ledger_account_id, ledger_account_id.to_vec(), ledger_account_id.to_hex(), ledger_subaccount, Vec::from(ledger_subaccount));
 
-    let user_principal_id = principal_from_str("yhy6j-huy54-mkzda-m26hc-yklb3-dzz4l-i2ykq-kr7tx-dhxyf-v2c2g-tae").unwrap();
+    // user, yhy6j-huy54-mkzda-m26hc-yklb3-dzz4l-i2ykq-kr7tx-dhxyf-v2c2g-tae
+    let user_principal_id: PrincipalId = principal_from_str("yhy6j-huy54-mkzda-m26hc-yklb3-dzz4l-i2ykq-kr7tx-dhxyf-v2c2g-tae").unwrap();
     let user_account_id: AccountIdentifier = AccountIdentifier::from(user_principal_id);
     let user_subaccount_1: Subaccount = Subaccount::from(&user_principal_id);
-    let user_subaccount_2: Subaccount = subaccount_from_principal(&user);
-    assert!(user_subaccount_1, user_subaccount_2);
+    let user_subaccount_2: Subaccount = subaccount_from_principal(&user_principal_id);
+    assert_eq!(user_subaccount_1, user_subaccount_2);
     println!("user principal id: {:?},\nprincipal id slice: {:?},\naccount id: {:?},\naccount id slice: {:?},\naccount id hex string: {:?},\nsubaccount: {:?},\nsubaccount slice: {:?},\n", 
-                user_principal_id, user_principal_id.as_slice(), user_account_id, user_account_id.to_vec(), user_account_id.to_hex(); user_subaccount_1, Vec::from(user_subaccount_1));
+                user_principal_id, user_principal_id.as_slice(), user_account_id, user_account_id.to_vec(), user_account_id.to_hex(), user_subaccount_1, Vec::from(user_subaccount_1));
 
     let ledger_user = AccountIdentifier::new(ledger_canister_id.get(), Some(user_subaccount_1));
     println!("ledger_user: {:?}, ledger_user string: {:?}", 
@@ -46,7 +47,6 @@ fn principal_from_str(input: &str) -> Result<PrincipalId, PrincipalIdParseError>
             if bytes.len() > PrincipalId::MAX_LENGTH_IN_BYTES + CRC_LENGTH_IN_BYTES {
                 return Err(PrincipalIdParseError::TooLong);
             }
-            println!("{:?}", bytes);
             let result =
                 try_from(&bytes.split_off(CRC_LENGTH_IN_BYTES)[..]).unwrap();
             let expected = format!("{}", result);
@@ -72,7 +72,6 @@ fn try_from(blob: &[u8]) -> Result<PrincipalId, PrincipalIdBlobParseError> {
     // }
     let mut data = [0u8; PrincipalId::MAX_LENGTH_IN_BYTES];
     data[..blob.len()].copy_from_slice(&blob[..]);
-    println!("{:?}\n", data);
     Ok(PrincipalId::new(blob.len(), data))
 }
 
